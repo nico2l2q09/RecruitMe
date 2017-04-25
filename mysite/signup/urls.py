df3832251fc4852cc60d4cab2a012de8b8cbadf2
nico2l2q09 <nbayless@princeton.edu>
2017-04-25 00:00:58 -0400

    woogogog

--- mysite/signup/urls.py
+++ mysite/signup/urls.py
@@ -6,8 +6,10 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^signupCoach/', views.signupCoach, name='signupCoach'),
     url(r'^signupPlayer/', views.signupPlayer, name='signupPlayer'),
-    url(r'^profile/', views.profile, name='profile'),
+    url(r'^profile/$', views.profile, name='profile'),
+    url(r'^profile/(?P<username>\w+$)', views.profile, name='profile'),
     url(r'^playerProfile/', views.playerProfile, name='playerProfile'),
+    url(r'^playerProfile/(?P<username>\w+$)', views.playerProfile, name='profile'),
     url(r'^exploreCoach/', views.exploreCoach, name='exploreCoach'),
     url(r'^explorePlayer/', views.explorePlayer, name='explorePlayer')
 ]
